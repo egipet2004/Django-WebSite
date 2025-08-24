587820ce02efde9789ab63318d9f91ce6cfab86b
egipet2004 <drax2k4@gmail.com>
2025-08-24 14:39:27 +0300

    Step 2

--- firstapp/mainpage/urls.py
+++ firstapp/mainpage/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Index, name='Home')
+    path('', views.Index, name='home'),
+    path('about', views.about, name='about')
 ]
--- firstapp/mainpage/views.py
+++ firstapp/mainpage/views.py
@@ -6,3 +6,7 @@ def Index(request):
     data = {'title': 'Главная страница '}
     return render(request, 'mainpage/index.html', data)
 
+def about(request):
+    data = {'title': 'Информация о разработчике'}
+    return render(request, 'mainpage/about.html', data)
+
